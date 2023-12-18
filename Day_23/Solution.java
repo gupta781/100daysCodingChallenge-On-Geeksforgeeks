0c4ac9cdb7d48e2e04f9b4a2a3b6c0257061aaf8
Vinayak Gupta <146059634+vinayakgupta781@users.noreply.github.com>
2023-12-19 02:11:01 +0530

    Add files via upload

--- Day_23/Solution.java
+++ Day_23/Solution.java
@@ -1,1 +1,18 @@
+class Solution
+{
+    private static int traverse(Node node,int k){
+        if(node == null) return 0;
+        if(node.data == k) {
+            return 1;
+        }
+        else if(node.data > k) return traverse(node.left,k);
+        else return traverse(node.right,k);
+    }
+    public static int countPairs(Node root1, Node root2, int x)
+    {
+        if(root1 == null) return 0;
+        return traverse(root2,x-root1.data) + countPairs(root1.left,root2,x) + countPairs(root1.right,root2,x);
+        // Code here
+    }
+}
 
