9f0dc178c67520b521a92f18d26b5543fab2a26c
Vinayak Gupta <146059634+vinayakgupta781@users.noreply.github.com>
2023-12-21 10:14:11 +0530

    Update Solution.java

--- Day_1/Solution.java
+++ Day_1/Solution.java
@@ -1,3 +1,4 @@
+\** 
 Question:- Given two arrays a[] and b[] of size n and m respectively. The task is to find the number of elements in the union between these two arrays.
 
 Union of the two arrays can be defined as the set containing distinct elements from both the arrays. If there are repetitions, then only one occurrence of element should be printed in the union.
@@ -17,6 +18,7 @@ Input:
 85 25 1 32 54 6
 85 2 
 Output: 7
+*/
 
 
 class Solution{
