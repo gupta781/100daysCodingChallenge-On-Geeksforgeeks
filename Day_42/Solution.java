70c8afc16a92e1f1cd17125ce736138a1bf36899
Vinayak Gupta <146059634+vinayakgupta781@users.noreply.github.com>
2024-01-16 11:32:30 +0530

    Update Solution.java

--- Day_42/Solution.java
+++ Day_42/Solution.java
@@ -1,27 +1,19 @@
-// A sorted(in ascending order) array A[ ] with distinct elements is rotated at some unknown point, the task is to find the minimum element in it.
+// Given a string S, check if it is palindrome or not.
 
-// Example 1
+// Example 1:
 
-// Input:
-// N = 5
-// arr[] = {4 ,5 ,1 ,2 ,3}
+// Input: S = "abba"
 // Output: 1
-// Explanation: 1 is the minimum element in the array.
+// Explanation: S is a palindrome
 
-class Solution
-{
-    int findMin(int a[], int n)
-    {
-        int ans =0;
-        int start=0,end = n-1;
-       
-        while(start<end){
-            int mid = start + (end-start)/2;
-            
-            if(a[mid]>a[end])  start=mid+1;
-            else end=mid;
+class Solution {
+    int isPalindrome(String S) {
+        char[] ch = S.toCharArray();
+        for(int i=0;i<=S.length()/2;i++){
+            if(S.charAt(i)!=S.charAt(S.length()-1 - i)){
+                return 0;
+            }
         }
-        
-        return a[start];
+        return 1;
     }
-}
+};
