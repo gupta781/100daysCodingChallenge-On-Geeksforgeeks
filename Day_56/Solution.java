7c1f9b91331b36b78000829bab5faef7c616ea00
Vinayak Gupta <146059634+vinayakgupta781@users.noreply.github.com>
2024-01-16 22:22:20 +0530

    Create Solution.java

---
+++ Day_56/Solution.java
@@ -0,0 +1,31 @@
+// Given a number N having only one ‘1’ and all other ’0’s in its binary representation, find position of the only set bit. If there are 0 or more than 1 set bit the answer should be -1. Position of  set bit '1' should be counted starting with 1 from LSB side in binary representation of the number.
+
+// Example 1:
+
+// Input:
+// N = 2
+// Output:
+// 2
+// Explanation:
+// 2 is represented as "10" in Binary.
+// As we see there's only one set bit
+// and it's in Position 2 and thus the
+// Output 2.
+
+
+class Solution {
+    static int findPosition(int N) {
+
+        if(((N-1)&N)!=0)
+        return -1;
+        int pos=1;
+        while(N!=0){
+            if((N&1)==1)
+            return pos;
+            else 
+            pos++;
+            N>>=1;
+        }
+        return -1;
+    }
+}
